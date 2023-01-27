8e15627bb7fc4a9b44694061e9eeff610f54c41d
ravenfahs <raven_fahs@hotmail.com>
2023-01-26 22:42:03 -0500

    Ciclo for

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+  Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y 6
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        for (int numeroFor=0; numeroFor<=6; numeroFor++){
+            System.out.println("El valor de la variable numeroFor en cada paso del ciclo for: "+numeroFor);
+        }
+    }
+}
