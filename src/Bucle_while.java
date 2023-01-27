834120150c195ef3dbf38d0864412b2e0eaed077
ravenfahs <raven_fahs@hotmail.com>
2023-01-27 01:06:12 -0500

    Ciclo_while

---
+++ src/Bucle_while.java
@@ -0,0 +1,13 @@
+/*
+* Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
+* */
+
+public class Bucle_while {
+    public static void main(String[] args) {
+        int nuemroWhilw =0;
+        while (nuemroWhilw<3){
+            System.out.println("Esta es la posición de la variable: "+ nuemroWhilw);
+            nuemroWhilw++;
+        }
+    }
+}
